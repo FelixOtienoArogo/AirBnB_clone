7d4ce40d38d0b5ea5b9657e2ca69b51f589a12c3
FelixOtienoArogo <felixarogo@gmail.com>
2022-11-28 00:42:48 +0300

    models

---
+++ models/base_model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This is the BaseModel to define all common methods for other classes
+"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    We house all common attributes of other classes
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        This is the constructor for this class
+
+        Arguments:
+            *args:list arguments
+            **kwargs: dictionary arguments
+        """
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        else:
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        """This prints the details about the object"""
+        return "[{}] ({}) <{}>".format(self.__class__.__name__,
+                                      self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        key_value = {}
+
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                key_value[key] = value.isoformat()
+            else:
+                key_value[key] = value
+
+        key_value["__class__"] = self.__class__.__name__
+        return key_value
