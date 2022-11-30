ccb33d36d6636e7af0cf1d6a5a6945f2835690e9
FelixOtienoArogo <felixarogo@gmail.com>
2022-11-30 17:03:05 +0300

    console3

--- console.py
+++ console.py
@@ -155,6 +155,40 @@ class HBNBCommand(cmd.Cmd):
 
         return value
 
+    def default(self, arg):
+        """
+        This looks for whether the command entered has the syntax:
+        "<class name>.<method name>" or not
+        """
+        if '.' in arg:
+            split = re.split(r'\.|\(|\)', arg)
+            class_name = split[0]
+            method_name = split[1]
+
+            if class_name in self.allowed_classes:
+                if method_name == 'all':
+                    print(self.get_objects(class_name))
+                elif method_name == 'count':
+                    print(len(self.get_objects(class_name)))
+                elif method_name == 'show':
+                    class_id = split[2][1:-1]
+                    self.do_show(class_name + ' ' + class_id)
+                elif method_name == 'destroy':
+                    class_id = split[2][1:-1]
+                    self.do_destroy(class_name + ' ' + class_id)
+
+    def get_objects(self, instance=''):
+        """
+        Gets the elements created by the console
+        """
+        objects = models.storage.all()
+
+        if instance:
+            keys = objects.keys()
+            return [str(val) for key, val in objects.items()
+                    if key.startswith(instance)]
+        return [str(val) for key, val in objects.items()]
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
